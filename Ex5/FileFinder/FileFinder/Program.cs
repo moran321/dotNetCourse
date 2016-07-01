32faaefebb2565ea3c1e41b1c31d6d3079b97d92
moran321 <moran.an@hotmail.com>
2016-07-01 11:40:38 +0300

    add

---
+++ Ex5/FileFinder/FileFinder/Program.cs
@@ -0,0 +1,38 @@
+﻿/* Moran Ankori */
+/* Lab 10.2 */
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Path.GetFullPath(args[0]); //get directory
+            Console.WriteLine(path);
+            string pattern = args[1];
+            
+            //get all files that match the pattern
+            string[] allFiles =  Directory.GetFiles(path, pattern, SearchOption.AllDirectories);
+
+           
+
+            //display files and their length
+            foreach (string file in allFiles)
+            {
+                FileInfo info = new FileInfo(file);
+                long length = info.Length;   
+                Console.WriteLine("File name: \"{0}\",\nLength: {1}\n",   Path.GetFileName(file), length);
+            }
+
+            Console.Read();
+
+        } /****end main *****************/
+
+    }/****end class *****************/
+}
